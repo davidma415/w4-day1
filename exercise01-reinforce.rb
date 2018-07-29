567620e574e2889fbdb812ae4c55adfe653e3c26
David Ma <davidma415@gmail.com>
2018-07-29 16:19:31 -0400

    Finished reinforcement part1

---
+++ exercise01-reinforce.rb
@@ -0,0 +1,20 @@
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+options = {
+  rows: 4,
+  cols: 4,
+  char: "*"
+}
+puts draw_shape(options)
